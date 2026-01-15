3efd251a9689ed295296508c89107d118666b8d3
AshtonAngel1 <158105094+AshtonAngel1@users.noreply.github.com>
2026-01-15 14:28:52 -0600

    Refactor curve_scores to use for loop
    Rewritten curve_scores function to use a for loop instead of map.

--- COSC3339_Inclass_One.py
+++ COSC3339_Inclass_One.py
@@ -60,7 +60,13 @@ def calculate_shipping_cost(weight, destination):
 
 # This method uses funky logic. Rewrite it using different loop structures
 def curve_scores(scores):
-    return list(map(lambda x: min(x + 5, 100), scores))
+    new_scores = []
+    for s in scores:
+        curved = s * 1.05
+        if curved > 100:
+            curved = 100
+        new_scores.append(curved)
+    return new_scores
 
 
 # For scenario three change the name of this method.
